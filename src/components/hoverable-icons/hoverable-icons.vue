36412fc95b95707a698c3049dda4ae72e96fc0b5
Parth Vyas <parth-vyas@outlook.com>
2023-03-18 19:12:14 -0600

    <test> krutarth changes

---
+++ src/components/hoverable-icons/hoverable-icons.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="icon-container">
+    <i class="icon"></i>
+  </div>
+</template>
+
+<style>
+.icon-container {
+  display: inline-block;
+  position: relative;
+  width: 50px;
+  height: 50px;
+}
+
+.icon {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  font-size: 24px;
+  text-align: center;
+  line-height: 50px;
+  transition: transform 0.2s ease-out;
+}
+
+.icon:hover {
+  transform: scale(1.2);
+}
+</style>
